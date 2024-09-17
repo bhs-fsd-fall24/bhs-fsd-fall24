24b89a5ede7eef460d9308c5b94d84cba8d4af82
Jorge Garro <jorge.garroelizondo@jpschools.org>
2024-09-17 16:09:49 +0000

    saving platformer

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -41,34 +41,73 @@ $(function () {
     
     // walls
     createPlatform(80, 0, 10, 625);
+    createPlatform(200, 500, 10, 125);
+    createPlatform(320, 125, 10, 250);
+    createPlatform(320, 625, 10, 125);
+    createPlatform(440, 0, 10, 250);
+    createPlatform(440, 375, 10, 125);
+    createPlatform(680, 125, 10, 250);
+    createPlatform(680, 500, 10, 125);
+    createPlatform(800, 375, 10, 250);
+    createPlatform(920, 0, 10, 125);
+    createPlatform(920, 500, 10, 250);
+    createPlatform(1040, 625, 10, 125);
+    createPlatform(1160, 125, 10, 250);
+    createPlatform(1160, 500, 10, 125);
+    createPlatform(1280, 125, 10, 500);
     
     // platforms
+    createPlatform(200, 125, 120, 10);
+    createPlatform(560, 125, 120, 10);
+    createPlatform(800, 125, 120, 10);
+    createPlatform(1145, 125, 40, 10);
+    createPlatform(1265, 125, 40, 10);
+    createPlatform(80, 250, 120, 10);
+    createPlatform(440, 250, 120, 10);
+    createPlatform(680, 250, 120, 10);
+    createPlatform(80, 375, 240, 10);
+    createPlatform(440, 375, 240, 10);
+    createPlatform(800, 375, 360, 10);
+    createPlatform(200, 500, 480, 10);
+    createPlatform(920, 500, 240, 10);
+    createPlatform(80, 625, 120, 10);
+    createPlatform(320, 625, 240, 10);
+    createPlatform(680, 625, 120, 10);
 
 
     // wedges
-  
-  
-
-
+    createPlatform(900, 625, 20, 10);
+    createPlatform(330, 313, 20, 10);
+    createPlatform(420, 188, 20, 10);
+    createPlatform(1290, 313, 20, 10);
+    createPlatform(1290, 563, 20 , 10);
+    createPlatform(1380, 438, 20 , 10);
+    createPlatform(1380, 188, 20 , 10);
+    createPlatform(1380, 688, 20 , 10);
     
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-    createCollectable("coin", 980, 563, 0, 0);
+    createCollectable("rock", 980, 563, 0, 0);
+    createCollectable("rock", 1220, 63, 0, 0);
+    createCollectable("rock", 500, 438, 0, 0);
+    createCollectable("rock", 860, 63, 0, 0);
+    createCollectable("rock", 140, 313, 0, 0);
+    createCollectable("rock", 140, 563, 0, 0);
 
-
-    
     // TODO 3
     // Create cannons
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
+    createCannon("top", 200, 1000, 20, 20);
+    createCannon("top", 550, 5000, 20, 20);
+    createCannon("left", 450, 3000, 20, 20);
+    createCannon("bottom", 760, 4000, 20, 20);
+    createCannon("bottom", 1000, 2000, 20, 20);
 
-
-
-    
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
     /////////////////////////////////////////////////
